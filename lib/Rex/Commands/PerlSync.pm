ebdb629639fc80e46c55e9afcf9df1730902f3ca
bbrtj <bbrtj.pro@gmail.com>
2024-10-01 19:18:10 +0200

    Tidy up

--- lib/Rex/Commands/PerlSync.pm
+++ lib/Rex/Commands/PerlSync.pm
@@ -16,333 +16,350 @@ use Rex::Helper::Path;
 use Rex::Helper::Encode;
 use Text::Glob 'match_glob';
 
-@EXPORT                            = qw(sync_up sync_down);
+@EXPORT = qw(sync_up sync_down);
 $Text::Glob::strict_wildcard_slash = 0;
 
-sub sync_up {
-  my ( $source, $dest, @option ) = @_;
+sub sync_up
+{
+	my ($source, $dest, @option) = @_;
 
-  my $options = {};
+	my $options = {};
 
-  if ( ref( $option[0] ) ) {
-    $options = $option[0];
-  }
-  else {
-    $options = {@option};
-  }
+	if (ref($option[0])) {
+		$options = $option[0];
+	}
+	else {
+		$options = {@option};
+	}
 
-  # default is, parsing templates (*.tpl) files
-  $options->{parse_templates} = TRUE;
+	# default is, parsing templates (*.tpl) files
+	$options->{parse_templates} = TRUE;
 
-  $source = resolv_path($source);
-  $dest   = resolv_path($dest);
+	$source = resolv_path($source);
+	$dest = resolv_path($dest);
 
-  #
-  # 0. normalize local path
-  #
-  $source = get_file_path( $source, caller );
+	#
+	# 0. normalize local path
+	#
+	$source = get_file_path($source, caller);
 
-  #
-  # first, build excludes list
-  #
+	#
+	# first, build excludes list
+	#
 
-  my $excludes = $options->{exclude} ||= [];
-  $excludes = [$excludes] unless ref($excludes) eq 'ARRAY';
+	my $excludes = $options->{exclude} ||= [];
+	$excludes = [$excludes] unless ref($excludes) eq 'ARRAY';
 
-  my @excluded_files = @{$excludes};
+	my @excluded_files = @{$excludes};
 
-  my $check_exclude_file = sub {
-    my ($file) = @_;
-    $file =~ s{^/}{};
+	my $check_exclude_file = sub {
+		my ($file) = @_;
+		$file =~ s{^/}{};
 
-    for my $cmp (@excluded_files) {
-      return 1 if match_glob( $cmp, $file );
-    }
+		for my $cmp (@excluded_files) {
+			return 1 if match_glob($cmp, $file);
+		}
 
-    return 0;
-  };
+		return 0;
+	};
 
-  #
-  # second, get all files on source side (minus excludes)
-  #
-  my @local_files = _get_local_files( $source, $check_exclude_file );
+	#
+	# second, get all files on source side (minus excludes)
+	#
+	my @local_files = _get_local_files($source, $check_exclude_file);
 
-  #
-  # third, get all files from destination side (minus excludes)
-  #
+	#
+	# third, get all files from destination side (minus excludes)
+	#
 
-  my @remote_files = _get_remote_files( $dest, $check_exclude_file );
+	my @remote_files = _get_remote_files($dest, $check_exclude_file);
 
-  #
-  # fourth, get the difference
-  #
+	#
+	# fourth, get the difference
+	#
 
-  my @diff = _diff_files( \@local_files, \@remote_files );
+	my @diff = _diff_files(\@local_files, \@remote_files);
 
-  #
-  # fifth, upload the different files
-  #
+	#
+	# fifth, upload the different files
+	#
 
-  my @uploaded_files;
-  for my $file (@diff) {
-    my ($dir)        = ( $file->{path} =~ m/(.*)\/[^\/]+$/ );
-    my ($remote_dir) = ( $file->{name} =~ m/\/(.*)\/[^\/]+$/ );
+	my @uploaded_files;
+	for my $file (@diff) {
+		my ($dir) = ($file->{path} =~ m/(.*)\/[^\/]+$/);
+		my ($remote_dir) = ($file->{name} =~ m/\/(.*)\/[^\/]+$/);
 
-    my ( %dir_stat, %file_stat );
-    LOCAL {
-      %dir_stat  = stat($dir);
-      %file_stat = stat( $file->{path} );
-    };
+		my (%dir_stat, %file_stat);
+		LOCAL {
+			%dir_stat = stat($dir);
+			%file_stat = stat($file->{path});
+		};
 
-    # check for overwrites
-    my %file_perm = ( mode => $file_stat{mode} );
-    if ( exists $options->{files} && exists $options->{files}->{mode} ) {
-      $file_perm{mode} = $options->{files}->{mode};
-    }
+		# check for overwrites
+		my %file_perm = (mode => $file_stat{mode});
+		if (exists $options->{files} && exists $options->{files}->{mode}) {
+			$file_perm{mode} = $options->{files}->{mode};
+		}
 
-    if ( exists $options->{files} && exists $options->{files}->{owner} ) {
-      $file_perm{owner} = $options->{files}->{owner};
-    }
+		if (exists $options->{files} && exists $options->{files}->{owner}) {
+			$file_perm{owner} = $options->{files}->{owner};
+		}
 
-    if ( exists $options->{files} && exists $options->{files}->{group} ) {
-      $file_perm{group} = $options->{files}->{group};
-    }
+		if (exists $options->{files} && exists $options->{files}->{group}) {
+			$file_perm{group} = $options->{files}->{group};
+		}
 
-    my %dir_perm = ( mode => $dir_stat{mode} );
-    if ( exists $options->{directories}
-      && exists $options->{directories}->{mode} )
-    {
-      $dir_perm{mode} = $options->{directories}->{mode};
-    }
+		my %dir_perm = (mode => $dir_stat{mode});
+		if (
+			exists $options->{directories}
+			&& exists $options->{directories}->{mode}
+			)
+		{
+			$dir_perm{mode} = $options->{directories}->{mode};
+		}
 
-    if ( exists $options->{directories}
-      && exists $options->{directories}->{owner} )
-    {
-      $dir_perm{owner} = $options->{directories}->{owner};
-    }
+		if (
+			exists $options->{directories}
+			&& exists $options->{directories}->{owner}
+			)
+		{
+			$dir_perm{owner} = $options->{directories}->{owner};
+		}
 
-    if ( exists $options->{directories}
-      && exists $options->{directories}->{group} )
-    {
-      $dir_perm{group} = $options->{directories}->{group};
-    }
-    ## /check for overwrites
+		if (
+			exists $options->{directories}
+			&& exists $options->{directories}->{group}
+			)
+		{
+			$dir_perm{group} = $options->{directories}->{group};
+		}
+		## /check for overwrites
 
-    if ($remote_dir) {
-      mkdir "$dest/$remote_dir", %dir_perm;
-    }
+		if ($remote_dir) {
+			mkdir "$dest/$remote_dir", %dir_perm;
+		}
 
-    Rex::Logger::debug(
-      "(sync_up) Uploading $file->{path} to $dest/$file->{name}");
-    if ( $file->{path} =~ m/\.tpl$/ && $options->{parse_templates} ) {
-      my $file_name = $file->{name};
-      $file_name =~ s/\.tpl$//;
+		Rex::Logger::debug(
+			"(sync_up) Uploading $file->{path} to $dest/$file->{name}"
+		);
+		if ($file->{path} =~ m/\.tpl$/ && $options->{parse_templates}) {
+			my $file_name = $file->{name};
+			$file_name =~ s/\.tpl$//;
 
-      file "$dest/" . $file_name,
-        content => template( $file->{path} ),
-        %file_perm;
+			file "$dest/" . $file_name,
+				content => template($file->{path}),
+				%file_perm;
 
-      push @uploaded_files, "$dest/$file_name";
-    }
-    else {
-      file "$dest/" . $file->{name},
-        source => $file->{path},
-        %file_perm;
+			push @uploaded_files, "$dest/$file_name";
+		}
+		else {
+			file "$dest/" . $file->{name},
+				source => $file->{path},
+				%file_perm;
 
-      push @uploaded_files, "$dest/" . $file->{name};
-    }
-  }
+			push @uploaded_files, "$dest/" . $file->{name};
+		}
+	}
 
-  if ( exists $options->{on_change}
-    && ref $options->{on_change} eq "CODE"
-    && scalar(@uploaded_files) > 0 )
-  {
-    Rex::Logger::debug("Calling on_change hook of sync_up");
-    $options->{on_change}->( map { $dest . $_->{name} } @diff );
-  }
-
-}
-
-sub sync_down {
-  my ( $source, $dest, @option ) = @_;
-
-  my $options = {};
-
-  if ( ref( $option[0] ) ) {
-    $options = $option[0];
-  }
-  else {
-    $options = {@option};
-  }
-
-  $source = resolv_path($source);
-  $dest   = resolv_path($dest);
-
-  #
-  # first, build excludes list
-  #
-
-  my $excludes = $options->{exclude} ||= [];
-  $excludes = [$excludes] unless ref($excludes) eq 'ARRAY';
-
-  my @excluded_files = @{$excludes};
-
-  my $check_exclude_file = sub {
-    my ($file) = @_;
-    $file =~ s{^/}{};
-
-    for my $cmp (@excluded_files) {
-      return 1 if match_glob( $cmp, $file );
-    }
-
-    return 0;
-  };
-
-  #
-  # second, get all files on dest side (minus excludes)
-  #
-  my @local_files = _get_local_files( $dest, $check_exclude_file );
-
-  #
-  # third, get all files from source side (minus excludes)
-  #
-
-  my @remote_files = _get_remote_files( $source, $check_exclude_file );
-
-  #
-  # fourth, get the difference
-  #
-
-  my @diff = _diff_files( \@remote_files, \@local_files );
-
-  #
-  # fifth, download the different files
-  #
-
-  for my $file (@diff) {
-    my ($dir)        = ( $file->{path} =~ m/(.*)\/[^\/]+$/ );
-    my ($remote_dir) = ( $file->{name} =~ m/\/(.*)\/[^\/]+$/ );
-
-    my ( %dir_stat, %file_stat );
-    %dir_stat  = stat($dir);
-    %file_stat = stat( $file->{path} );
-
-    LOCAL {
-      if ($remote_dir) {
-        mkdir "$dest/$remote_dir", mode => $dir_stat{mode};
-      }
-    };
-
-    Rex::Logger::debug(
-      "(sync_down) Downloading $file->{path} to $dest/$file->{name}");
-    download( $file->{path}, "$dest/$file->{name}" );
-
-    LOCAL {
-      chmod $file_stat{mode}, "$dest/$file->{name}";
-    };
-  }
-
-  if ( exists $options->{on_change}
-    && ref $options->{on_change} eq "CODE"
-    && scalar(@diff) > 0 )
-  {
-    Rex::Logger::debug("Calling on_change hook of sync_down");
-    if ( substr( $dest, -1 ) eq "/" ) {
-      $dest = substr( $dest, 0, -1 );
-    }
-    $options->{on_change}->( map { $dest . $_->{name} } @diff );
-  }
+	if (
+		exists $options->{on_change}
+		&& ref $options->{on_change} eq "CODE"
+		&& scalar(@uploaded_files) > 0
+		)
+	{
+		Rex::Logger::debug("Calling on_change hook of sync_up");
+		$options->{on_change}->(map { $dest . $_->{name} } @diff);
+	}
 
 }
 
-sub _get_local_files {
-  my ( $source, $exclude_sub ) = @_;
+sub sync_down
+{
+	my ($source, $dest, @option) = @_;
 
-  if ( !-d $source ) { die("$source : no such directory."); }
+	my $options = {};
 
-  my @dirs = ($source);
-  my @local_files;
-  LOCAL {
-    for my $dir (@dirs) {
-      for my $entry ( list_files($dir) ) {
-        next if ( $entry eq "." );
-        next if ( $entry eq ".." );
+	if (ref($option[0])) {
+		$options = $option[0];
+	}
+	else {
+		$options = {@option};
+	}
 
-        my $name = "$dir/$entry";
-        $name =~ s/^\Q$source\E//;
-        next if $exclude_sub->($name);
+	$source = resolv_path($source);
+	$dest = resolv_path($dest);
 
-        if ( is_dir("$dir/$entry") ) {
-          push( @dirs, "$dir/$entry" );
-          next;
-        }
-        push(
-          @local_files,
-          {
-            name => $name,
-            path => "$dir/$entry",
-            md5  => md5("$dir/$entry"),
-          }
-        );
+	#
+	# first, build excludes list
+	#
 
-      }
-    }
-  };
+	my $excludes = $options->{exclude} ||= [];
+	$excludes = [$excludes] unless ref($excludes) eq 'ARRAY';
 
-  return @local_files;
+	my @excluded_files = @{$excludes};
+
+	my $check_exclude_file = sub {
+		my ($file) = @_;
+		$file =~ s{^/}{};
+
+		for my $cmp (@excluded_files) {
+			return 1 if match_glob($cmp, $file);
+		}
+
+		return 0;
+	};
+
+	#
+	# second, get all files on dest side (minus excludes)
+	#
+	my @local_files = _get_local_files($dest, $check_exclude_file);
+
+	#
+	# third, get all files from source side (minus excludes)
+	#
+
+	my @remote_files = _get_remote_files($source, $check_exclude_file);
+
+	#
+	# fourth, get the difference
+	#
+
+	my @diff = _diff_files(\@remote_files, \@local_files);
+
+	#
+	# fifth, download the different files
+	#
+
+	for my $file (@diff) {
+		my ($dir) = ($file->{path} =~ m/(.*)\/[^\/]+$/);
+		my ($remote_dir) = ($file->{name} =~ m/\/(.*)\/[^\/]+$/);
+
+		my (%dir_stat, %file_stat);
+		%dir_stat = stat($dir);
+		%file_stat = stat($file->{path});
+
+		LOCAL {
+			if ($remote_dir) {
+				mkdir "$dest/$remote_dir", mode => $dir_stat{mode};
+			}
+		};
+
+		Rex::Logger::debug(
+			"(sync_down) Downloading $file->{path} to $dest/$file->{name}"
+		);
+		download($file->{path}, "$dest/$file->{name}");
+
+		LOCAL {
+			chmod $file_stat{mode}, "$dest/$file->{name}";
+		};
+	}
+
+	if (
+		exists $options->{on_change}
+		&& ref $options->{on_change} eq "CODE"
+		&& scalar(@diff) > 0
+		)
+	{
+		Rex::Logger::debug("Calling on_change hook of sync_down");
+		if (substr($dest, -1) eq "/") {
+			$dest = substr($dest, 0, -1);
+		}
+		$options->{on_change}->(map { $dest . $_->{name} } @diff);
+	}
+
 }
 
-sub _get_remote_files {
-  my ( $dest, $exclude_sub ) = @_;
+sub _get_local_files
+{
+	my ($source, $exclude_sub) = @_;
 
-  if ( !is_dir($dest) ) { die("$dest : no such directory."); }
+	if (!-d $source) { die("$source : no such directory."); }
 
-  my @remote_dirs = ($dest);
-  my @remote_files;
+	my @dirs = ($source);
+	my @local_files;
+	LOCAL {
+		for my $dir (@dirs) {
+			for my $entry (list_files($dir)) {
+				next if ($entry eq ".");
+				next if ($entry eq "..");
 
-  for my $dir (@remote_dirs) {
-    for my $entry ( list_files($dir) ) {
-      next if ( $entry eq "." );
-      next if ( $entry eq ".." );
+				my $name = "$dir/$entry";
+				$name =~ s/^\Q$source\E//;
+				next if $exclude_sub->($name);
 
-      my $name = "$dir/$entry";
-      $name =~ s/^\Q$dest\E//;
-      next if $exclude_sub->($name);
+				if (is_dir("$dir/$entry")) {
+					push(@dirs, "$dir/$entry");
+					next;
+				}
+				push(
+					@local_files,
+					{
+						name => $name,
+						path => "$dir/$entry",
+						md5 => md5("$dir/$entry"),
+					}
+				);
 
-      if ( is_dir("$dir/$entry") ) {
-        push( @remote_dirs, "$dir/$entry" );
-        next;
-      }
+			}
+		}
+	};
 
-      push(
-        @remote_files,
-        {
-          name => $name,
-          path => "$dir/$entry",
-          md5  => md5("$dir/$entry"),
-        }
-      );
-    }
-  }
-
-  return @remote_files;
+	return @local_files;
 }
 
-sub _diff_files {
-  my ( $files1, $files2 ) = @_;
-  my @diff;
+sub _get_remote_files
+{
+	my ($dest, $exclude_sub) = @_;
 
-  for my $file1 ( @{$files1} ) {
-    my @data = grep {
-           ( $_->{name} eq $file1->{name} )
-        && ( $_->{md5} eq $file1->{md5} )
-    } @{$files2};
-    if ( scalar @data == 0 ) {
-      push( @diff, $file1 );
-    }
-  }
+	if (!is_dir($dest)) { die("$dest : no such directory."); }
 
-  return @diff;
+	my @remote_dirs = ($dest);
+	my @remote_files;
+
+	for my $dir (@remote_dirs) {
+		for my $entry (list_files($dir)) {
+			next if ($entry eq ".");
+			next if ($entry eq "..");
+
+			my $name = "$dir/$entry";
+			$name =~ s/^\Q$dest\E//;
+			next if $exclude_sub->($name);
+
+			if (is_dir("$dir/$entry")) {
+				push(@remote_dirs, "$dir/$entry");
+				next;
+			}
+
+			push(
+				@remote_files,
+				{
+					name => $name,
+					path => "$dir/$entry",
+					md5 => md5("$dir/$entry"),
+				}
+			);
+		}
+	}
+
+	return @remote_files;
+}
+
+sub _diff_files
+{
+	my ($files1, $files2) = @_;
+	my @diff;
+
+	for my $file1 (@{$files1}) {
+		my @data = grep {
+			($_->{name} eq $file1->{name})
+				&& ($_->{md5} eq $file1->{md5})
+		} @{$files2};
+		if (scalar @data == 0) {
+			push(@diff, $file1);
+		}
+	}
+
+	return @diff;
 }
 
 1;
--- t/base.t
+++ t/base.t
@@ -12,117 +12,124 @@ use Rex::Task;
 ################################################################################
 
 subtest 'should sync_up' => sub {
-  my $source = 't/sync';
-  my $target = prepare_directory();
+	my $source = 't/sync';
+	my $target = prepare_directory();
 
-  run_task(
-    sub {
-      sync_up $source, $target;
-    }
-  );
+	run_task(
+		sub {
+			sync_up $source, $target;
+		}
+	);
 
-  compare_contents( $source, $target );
+	compare_contents($source, $target);
 };
 
 subtest 'should sync_up with excludes' => sub {
-  my $source = 't/sync';
-  my $target = prepare_directory();
+	my $source = 't/sync';
+	my $target = prepare_directory();
 
-  # NOTE: file4 should not be excluded, as it is nested
-  run_task(
-    sub {
-      sync_up $source, $target,
-        { exclude => [ 'dir/file2', 'file4', 'dir with spaces' ] };
-    }
-  );
+	# NOTE: file4 should not be excluded, as it is nested
+	run_task(
+		sub {
+			sync_up $source, $target,
+				{exclude => ['dir/file2', 'file4', 'dir with spaces']};
+		}
+	);
 
-  compare_contents( $source, $target,
-    [ '/dir/file2', '/dir with spaces', '/dir with spaces/file3' ] );
+	compare_contents(
+		$source, $target,
+		['/dir/file2', '/dir with spaces', '/dir with spaces/file3']
+	);
 };
 
 subtest 'should sync_down' => sub {
-  my $source = 't/sync';
-  my $target = prepare_directory();
+	my $source = 't/sync';
+	my $target = prepare_directory();
 
-  run_task(
-    sub {
-      sync_down $source, $target;
-    }
-  );
+	run_task(
+		sub {
+			sync_down $source, $target;
+		}
+	);
 
-  compare_contents( $source, $target );
+	compare_contents($source, $target);
 };
 
 subtest 'should sync_down with excludes' => sub {
-  my $source = 't/sync';
-  my $target = prepare_directory();
+	my $source = 't/sync';
+	my $target = prepare_directory();
 
-  # NOTE: file4 should not be excluded, as it is nested
-  run_task(
-    sub {
-      sync_down $source, $target,
-        { exclude => [ 'dir/file2', 'file4', 'dir with spaces' ] };
-    }
-  );
+	# NOTE: file4 should not be excluded, as it is nested
+	run_task(
+		sub {
+			sync_down $source, $target,
+				{exclude => ['dir/file2', 'file4', 'dir with spaces']};
+		}
+	);
 
-  compare_contents( $source, $target,
-    [ '/dir/file2', '/dir with spaces', '/dir with spaces/file3' ] );
+	compare_contents(
+		$source, $target,
+		['/dir/file2', '/dir with spaces', '/dir with spaces/file3']
+	);
 };
 
-sub prepare_directory {
-  my $target = tempdir( CLEANUP => 1 );
-  die unless -d $target;
+sub prepare_directory
+{
+	my $target = tempdir(CLEANUP => 1);
+	die unless -d $target;
 
-  return $target;
+	return $target;
 }
 
-sub run_task {
-  my ($func) = @_;
+sub run_task
+{
+	my ($func) = @_;
 
-  my $task = Rex::Task->new(
-    name => 'sync_test',
-    func => $func,
-  );
+	my $task = Rex::Task->new(
+		name => 'sync_test',
+		func => $func,
+	);
 
-  $task->run('<local>');
+	$task->run('<local>');
 }
 
-sub compare_contents {
-  my ( $source, $target, $excluded ) = @_;
-  $excluded //= [];
-  my %excluded_map = map { $_ => 1 } @{$excluded};
+sub compare_contents
+{
+	my ($source, $target, $excluded) = @_;
+	$excluded //= [];
+	my %excluded_map = map { $_ => 1 } @{$excluded};
 
-  # test sync results
-  my ( @expected, @result );
+	# test sync results
+	my (@expected, @result);
 
-  # expected results
-  find(
-    {
-      wanted => sub {
-        s/$source//;
-        return unless length;
-        return if $excluded_map{$_};
-        push @expected, $_;
-      },
-      no_chdir => 1
-    },
-    $source
-  );
+	# expected results
+	find(
+		{
+			wanted => sub {
+				s/$source//;
+				return unless length;
+				return if $excluded_map{$_};
+				push @expected, $_;
+			},
+			no_chdir => 1
+		},
+		$source
+	);
 
-  # actual results
-  find(
-    {
-      wanted => sub {
-        s/$target//;
-        return unless length;
-        push @result, $_;
-      },
-      no_chdir => 1
-    },
-    $target
-  );
+	# actual results
+	find(
+		{
+			wanted => sub {
+				s/$target//;
+				return unless length;
+				push @result, $_;
+			},
+			no_chdir => 1
+		},
+		$target
+	);
 
-  is( \@result, bag {item $_ for @expected}, 'synced dir matches' );
+	is(\@result, bag { item $_ for @expected }, 'synced dir matches');
 }
 
 done_testing;
